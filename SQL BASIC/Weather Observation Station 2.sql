ea9773e30f74aee9df59a6d5b02c69e621507cdb
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-15 17:02:32 -0500

    Create Weather Observation Station 2.sql

---
+++ SQL BASIC/Weather Observation Station 2.sql
@@ -0,0 +1,21 @@
+/*
+     SKILL: 	SQL (Basic)
+DIFFICULTY: 	Easy
+ SUBDOMAIN: 	Aggregation
+ CHALLENGE: 	Weather Observation Station 2
+
+*********************************************************************************
+*/
+CHALLENGE QUE :
+Query the following two values from the STATION table:
+
+The sum of all values in LAT_N rounded to a scale of  decimal places.
+The sum of all values in LONG_W rounded to a scale of  decimal places.
+  
+
+SOLUTION :
+
+SELECT
+cast(ROUND(Sum(LAT_N), 2) as Decimal(10,2)),
+cast(ROUND(sum(LONG_W), 2) as Decimal(10,2))
+FROM STATION;
