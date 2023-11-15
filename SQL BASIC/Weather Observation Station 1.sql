37333df7133c5b91180def70548152c6ea6a4c36
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-15 17:05:59 -0500

    Update and rename Weather Observation Station 1 to Weather Observation Station 1.sql

---
+++ SQL BASIC/Weather Observation Station 1.sql
@@ -0,0 +1,15 @@
+/*
+     SKILL: 	SQL (Basic)
+DIFFICULTY: 	Easy
+ SUBDOMAIN: 	Basic Select
+ CHALLENGE: 	Weather Observation Station 1
+*********************************************************************************
+*/
+CHALLENGE QUE:
+Query a list of CITY and STATE from the STATION table.
+
+
+SOLUTION :
+
+SELECT City, State
+FROM Station;
