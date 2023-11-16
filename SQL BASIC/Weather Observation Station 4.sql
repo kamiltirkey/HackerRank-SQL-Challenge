6537ee2afb356a9dd03c627fd264d57cdf713be2
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-16 16:05:46 -0500

    Update Weather Observation Station 4.sql

--- SQL BASIC/Weather Observation Station 4.sql
+++ SQL BASIC/Weather Observation Station 4.sql
@@ -1,8 +1,9 @@
+
+CHALLNGE QUE :
 Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 The STATION table is described as follows:
 
 
-  
 Station.jpg
 
 
