9e6b57d01ca74632e28ef5a00db9f20f7fdaed43
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-16 15:03:41 -0500

    Create Weather Observation Station 3.sql

---
+++ SQL BASIC/Weather Observation Station 3.sql
@@ -0,0 +1,7 @@
+CHALLENGE QUE :
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+
+SOLUTION :
+Select Distinct CITY
+From Station
+Where ID%2=0;
