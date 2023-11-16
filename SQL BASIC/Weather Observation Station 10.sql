17eb04e25181919a1ecd2967d6f530e19efee845
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-16 16:58:17 -0500

    Create Weather Observation Station 10.sql

---
+++ SQL BASIC/Weather Observation Station 10.sql
@@ -0,0 +1,17 @@
+
+CHALLANGE QUE :
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+
+  
+SOLUTION :
+  
+SELECT DISTINCT (CITY)
+FROM STATION
+WHERE city NOT LIKE '%[a,e,i,o,u,A,E,I,O,U]'
+ORDER BY CITY;
+
+----------------------------------------------------
+Submitted Code
+Language: MS SQL
+====================================================
