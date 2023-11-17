cc84af49f9d52f697b6d88b3b36a3437e809fbe2
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-17 14:46:28 -0500

    Create Employee Names.sql

---
+++ SQL BASIC/Employee Names.sql
@@ -0,0 +1,14 @@
+
+CHALLENGE QUE :
+/*Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.*/
+
+    
+SOLULTION :
+    
+SELECT Name
+FROM Employee
+Order By Name ASC;
+
+--------------
+Submitted Code
+Language: MS SQL
