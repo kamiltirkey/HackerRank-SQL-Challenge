0a7fb70d50265afaf71e8db77f04ed272b19e7f6
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-18 13:10:19 -0500

    Create Population Density Difference.sql

---
+++ SQL BASIC/Population Density Difference.sql
@@ -0,0 +1,13 @@
+CHALLENGE QUE :
+/***Query the difference between the maximum and minimum populations in CITY.***/
+
+SOLUTION :
+
+SELECT MAX(POPULATION)-MIN(POPULATION) AS DIFFERENCE_POPULATION
+FROM CITY
+
+
+---------------------------------
+
+Submitted Code
+Language: MS SQL
