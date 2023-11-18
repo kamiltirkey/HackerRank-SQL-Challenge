81909c6958f825a02af244c3efb54d58a7307bfa
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-18 13:05:49 -0500

    Create Revising Aggregations - Averages.sql

---
+++ SQL BASIC/Revising Aggregations - Averages.sql
@@ -0,0 +1,16 @@
+CHALLENGE QUE :
+
+/** Query the average population of all cities in CITY where District is California.**/
+
+
+SOLUTION :
+  
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE  DISTRICT = 'CALIFORNIA'
+
+
+-------------------------
+
+Submitted Code
+Language: MS SQL
