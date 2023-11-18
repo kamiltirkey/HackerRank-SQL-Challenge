d33b86f81296512bea263f92af3739d38d308405
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2023-11-18 13:07:16 -0500

    Create Average Population.sql

---
+++ SQL BASIC/Average Population.sql
@@ -0,0 +1,13 @@
+CHALLENGE QUE :
+
+/***Query the average population for all cities in CITY, rounded down to the nearest integer.***/
+
+SOLUTION :
+
+SELECT FLOOR(AVG(POPULATION))
+FROM CITY
+
+---------------------------
+  
+Submitted Code
+Language: MS SQL
